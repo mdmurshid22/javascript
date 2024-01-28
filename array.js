ae61d5a879fd4ebbbc6e6fddc935b39424f11d38
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-01-28 21:44:50 +0530

    practise folder is updated!

--- array.js
+++
@@ -1,2 +0,0 @@
-let arr=new Array(["Idiot",6.8,"k"],[81,false],[true],["h","K"])
-console.log(arr)
--- test.js
+++ test.js
@@ -3,3 +3,7 @@ while (i <= 26){
 	document.write(i+'<br>')
 	i+=2
 }
+for(i=0; i<=10; i+=2)
+{
+	console.log(i)
+}
