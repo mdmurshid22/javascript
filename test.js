84afe7b60d3b119553b148ab087b5e0bc8d6ff90
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-01-29 22:53:21 +0530

    update javascript coding!

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
-num=Number(prompt("Enter Any Number"))
+/*num=Number(prompt("Enter Any Number"))
 for(i=1;i<=num;i++){
 	for(j=1;j<=i;j++){
 	document.write('*')
 }
+document.write('<br>')
+}
+document.write(typeof(num))*/
+num1=Number(prompt("Enter Any Number"))
+var i=1
+let j=1
+while (i<=num1){
+	while (j<=num1){
+	document.write("$")
+	j++
+}
+i++
 document.write('<br>')
 }
