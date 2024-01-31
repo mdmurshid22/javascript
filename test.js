524ca3472fddd7f03c4020564ba9284759a8698d
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-01-31 13:40:08 +0530

    JavaScript coding practise!

--- test.js
+++ test.js
@@ -6,15 +6,4 @@ for(i=1;i<=num;i++){
 document.write('<br>')
 }
 document.write(typeof(num))*/
-num1=Number(prompt("Enter Any Number"))
-var i=1
-var j=1
-while (i<=num1){
-	while (j<=i){
-	document.write('*'+" ")
-	j++
-}
-document.write('<br>')
-i++
-j=1
-}
+num=Number(prompt("Enter Any Number"))
