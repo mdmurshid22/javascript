8ee460c1c7a5b77aadd5d6ca14e5cbca0ec7f527
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-07-15 18:42:40 +0530

    updated code!

--- two.js
+++ two.js
@@ -1,2 +1,18 @@
-console.log('Allah kodudha en hujjathul baliga')
-console.log('kuttyma')
+/* while ang for loop */
+/*i = 1
+while(i<=10){
+	console.log(i)
+	i+=1
+}*/
+/*name = prompt('enter my heart:')
+while(name!='idiot'){
+	name = prompt('enter my sweet:')
+}
+console.log(name)*/
+/*num = prompt("Enter some Number:")
+if(num%2 == 0){
+	console.log("Even Number:",num)
+}
+else{
+	console.log('Odd Number:',num)
+}*/
