5feef7c1a7ea543cc222e793e1c375a2bfd4ed55
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-08-06 22:25:17 +0530

    Square star pattern program in JavaScript by using for loop.

--- pattern.js
+++ pattern.js
@@ -1,30 +1,25 @@
 //1)Square star pattern in javascript.
 //Eg ->1. by using repeat() method.
-let num = 5
+
+/*let num = 5
 let star = '* '
 for(i=1;i<=num;i+=1){
 	let s = star.repeat(num);
 	console.log(s)
+}*/
+//-------------------------------------------------------------------------------------------
+
+//Eg ->2. by using for loop.
+
+let num = 5
+let result = ''
+for(i=1;i<=num;i++){
+	for(j=1;j<=num;j++){
+		result = result + '* '
+	}
+	result = result + '\n'
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(result)
 
 
 
