43b8d27429237b0f2c56e276870a2f009204385b
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-08-18 17:03:47 +0530

    JS code is updated!

--- func.js
+++ func.js
@@ -1,8 +1,2 @@
-const obj1 = {name1:'idiot',age1:21,address1:'pavai'}
-const obj2 = {name2:'k',age2:22,address2:'karai'}
-
-function details(obj){
-	console.log(obj)
-}
-details(obj1.age1)
-details(obj2.name2)
+const numbr = () => FirstName + ' ' + LastName
+console.log('idiot','k')
