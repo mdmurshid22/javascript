5cf684f8189633866e2ae03137f07bc40302f742
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-08-18 15:10:20 +0530

    Function Concept code is updated!

--- func.js
+++ func.js
@@ -1,7 +1,8 @@
-const obj = {name:'idiot',age:21,address:'pavai'}
+const obj1 = {name1:'idiot',age1:21,address1:'pavai'}
+const obj2 = {name2:'k',age2:22,address2:'karai'}
 
 function details(obj){
 	console.log(obj)
 }
-details(obj.age)
-details(obj.name)
+details(obj1.age1)
+details(obj2.name2)
